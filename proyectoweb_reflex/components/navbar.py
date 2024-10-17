c66c9b52bb867caec6d8652dac0dedb4f49c2358
sefogi <sebast18@gmai.com>
2024-10-17 12:46:21 +0200

    creacion de paginas principales

---
+++ proyectoweb_reflex/components/navbar.py
@@ -0,0 +1,5 @@
+import reflex as rx 
+
+
+def navbar()-> rx.Component:
+    return 
--- proyectoweb_reflex/proyectoweb_reflex.py
+++ proyectoweb_reflex/proyectoweb_reflex.py
@@ -1,1 +1,24 @@
 
+import reflex as rx 
+
+class State(rx.State):
+    pass
+
+
+def index() -> rx.Component:
+    
+    return rx.center(
+        rx.text("hola reflex", color="blue", text_align="center",font_size="2em")
+        
+    )
+
+
+
+
+
+
+app= rx.App()
+app.add_page(index)
+app._compile()
+
+
